bd804594e9bf6acd5308714391fe959543108211
laurentdelay <laurent.delay89@gmail.com>
2020-12-22 18:42:43 +0100

    récupération des postes dans l'ordre décroissant

--- graphql/resolvers/postsResolvers.js
+++ graphql/resolvers/postsResolvers.js
@@ -11,7 +11,7 @@ module.exports = {
     // récupération de l'ensemble des posts
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
 
         return posts;
       } catch (error) {
